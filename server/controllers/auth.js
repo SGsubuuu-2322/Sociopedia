6975e0f21acf3436fe6dbe8b91d84320e7b18645
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-20 20:49:44 +0530

    Created the registration controller's action for the user...

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,39 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const register = (req, res) => {
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      location,
+      friends,
+      occupation,
+    } = req.body;
 
-}
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const User = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashPassword,
+      picturePath,
+      location,
+      friends,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 100000),
+      impressions: Math.floor(Math.random() * 100000),
+    });
+
+    const savedUser = await User.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    console.log(err);
+  }
+};
