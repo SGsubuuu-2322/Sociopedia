cf297085b3e979cb7649416319fa4edb84a45a14
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-20 20:05:43 +0530

    Added the basic setup for our server and get started with suthentication...

--- server/index.js
+++ server/index.js
@@ -7,5 +7,46 @@ import dotenv from "dotenv";
 import multer from "multer";
 import morgan from "morgan";
 import path from "path";
-import { pathToFileURL } from "url";
+import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
+// Configuration for our express server app...
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// File Storage
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Auth routing
+
+app.post("/auth/route", upload.single("picture"), register);
+
+// MongoDB setup...
+const PORT = process.env.SERVER_PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(`Error connecting to database: ${err}`));
