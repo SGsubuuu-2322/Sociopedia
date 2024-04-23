f524804bbf8bb6adf1c0e733683df2fce3a32b02
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-23 23:48:46 +0530

    Created the getUserFriends controller's action...for fetching user all friends...

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -12,3 +12,32 @@ export const getUser = async (req, res) => {
     return res.status(404).json({ error: err.message });
   }
 };
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      const friends = await Promise.all(
+        user.friends.map((id) => User.findById(id))
+      );
+      const formattedFriends = friends.map(
+        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+          return {
+            _id,
+            firstName,
+            lastName,
+            occupation,
+            location,
+            picturePath,
+          };
+        }
+      );
+
+      return res.status(200).json(formattedFriends);
+    }
+
+    return res.status(404).json({ error: "User not found" });
+  } catch (err) {
+    return res.status(404).json({ error: err.message });
+  }
+};
