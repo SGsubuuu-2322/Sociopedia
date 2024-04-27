c71594532b67383924a7ff0c5777566d23035490
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-28 00:31:28 +0530

    Added the likeunlike posts controllers action for our app...

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,7 @@
 import Post from "../models/Posts.js";
 import User from "../models/User.js";
 
+// Create operation...
 export const createPost = async (req, res) => {
   try {
     const { userId, description, picturePath } = req.body;
@@ -24,6 +25,7 @@ export const createPost = async (req, res) => {
   }
 };
 
+// Read Operations...
 export const getPostFeeds = async (req, res) => {
   try {
     const posts = await Post.find();
@@ -38,6 +40,33 @@ export const getUserPosts = async (req, res) => {
     const { userId } = req.params;
     const posts = await Post.find({ userId });
     return res.status(201).json(posts);
+  } catch (err) {
+    return res.status(404).json({ message: err.message });
+  }
+};
+
+// Updata Operations....
+export const likePosts = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const post = await Post.findById(id);
+    const isLiked = await post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    return res.status(200).json({ updatedPost });
   } catch (err) {
     return res.status(404).json({ message: err.message });
   }
