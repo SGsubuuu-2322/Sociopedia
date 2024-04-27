264dea44db536da5ce71d17778ac4331e7bda748
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-27 09:58:07 +0530

    Created the post creation controller action...

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,25 @@
-import Post from "../models/Posts.js"
+import Post from "../models/Posts.js";
+import User from "../models/User.js";
+
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      userPicturePath: user.picturePath,
+      description,
+      picturePath,
+      likes: {},
+      comments: [],
+    });
+    await newPost.save();
+    const posts = await Post.find();
+    return res.status(201).json(posts);
+  } catch (err) {
+    return res.status(409).json({ message: err.message });
+  }
+};
