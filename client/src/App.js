3a67cce760663bf2826c77ad4f20f385b752ab0f
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-29 12:55:36 +0530

    Added multiple page routing setup...

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,19 @@
+import HomePage from "scenes/homePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:id" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
